444fa8b92434f9e362d1d64a8a420580ce5b4af1
JuanFVO <JuanFVazquezOlivas@gmail.com>
2026-06-10 22:51:52 -0600

    Added pagination logic

--- scripts/ingestion/extract_resources.py
+++ scripts/ingestion/extract_resources.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import time
 from pathlib import Path
 from typing import Dict, List, Optional
 
@@ -24,19 +25,40 @@ def saveJson(data: Dict, outputPath: Path) -> None:
     with open(outputPath, "w", encoding="utf-8") as file:
         json.dump(data, file, indent=2)
 
-def extractResource(resourceType: str) -> None:
+def getNextUrl(bundle: Dict) -> Optional[str]:
+    links: List[Dict] = bundle.get("link", [])
+
+    for link in links:
+        if link.get("relation") == "next":
+            return link.get("url")
+        
+    return None
+
+
+
+def extractResource(resourceType: str, pageSize: int = 20) -> None:
     
     resourceOutputDir = OUTPUT_DIR / resourceType.lower()
     resourceOutputDir.mkdir(parents=True, exist_ok=True)
 
     url = f"{FHIR_BASE_URL}/{resourceType}"
-    
-    response = requests.get(url)
-    response.raise_for_status()
-    bundle = response.json()
+    params = {"_count": pageSize}
 
-    outPath = resourceOutputDir / f"{resourceType.lower()}.json"
-    saveJson(bundle, outPath)
+    pageNum = 1
+
+    while url:
+    
+        response = requests.get(url, params=params if pageNum ==  1 else None, timeout=60)
+        response.raise_for_status()
+        bundle = response.json()
+
+        outPath = resourceOutputDir / f"{resourceType.lower()}_page_{pageNum}.json"
+        saveJson(bundle, outPath)
+
+        url = getNextUrl(bundle)
+        pageNum += 1
+        params = None
+        time.sleep(0.2)
 
 def main() -> None:
     for resource in resources:
