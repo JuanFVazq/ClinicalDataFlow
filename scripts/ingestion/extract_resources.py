01666b8af2427f6365973e53da793f86daf521b7
JuanFVO <JuanFVazquezOlivas@gmail.com>
2026-06-10 22:03:59 -0600

    Succesfully Extracted Data from FHIR API

--- scripts/ingestion/extract_resources.py
+++ scripts/ingestion/extract_resources.py
@@ -1,10 +1,9 @@
 import json
-import time
 import requests
 from pathlib import Path
 from typing import Dict, List, Optional
 
-FHIR_BASE_URL = "https://localhost:8080/fhir"
+FHIR_BASE_URL = "http://localhost:8080/fhir"
 OUTPUT_DIR = Path("data/raw/fhir_api")
 
 resources = [
@@ -13,12 +12,38 @@ resources = [
     "Condition",
     "Observation",
     "MedicationRequest",
+    "Procedure",
     "AllergyIntolerance",
-    "Immunization"
+    "Immunization",
 ]
+
 
 def saveJson(data: Dict, outputPath: Path) -> None:
     outputPath.parent.mkdir(parents=True, exist_ok=True)
 
     with open(outputPath, "w", encoding="utf-8") as file:
         json.dump(data, file, indent=2)
+
+def extractResource(resourceType: str) -> None:
+    
+    resourceOutputDir = OUTPUT_DIR / resourceType.lower()
+    resourceOutputDir.mkdir(parents=True, exist_ok=True)
+
+    url = f"{FHIR_BASE_URL}/{resourceType}"
+    
+    response = requests.get(url)
+    response.raise_for_status()
+    bundle = response.json()
+
+    outPath = resourceOutputDir / f"{resourceType.lower()}.json"
+    saveJson(bundle, outPath)
+
+def main() -> None:
+    for resource in resources:
+        try:
+            extractResource(resource)
+        except Exception as error:
+            print(error)
+
+if __name__ == "__main__":
+    main()
