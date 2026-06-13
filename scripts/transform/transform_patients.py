8a9c4ff15b1a186bc03d868fe270ce2e937a329e
JuanFVO <JuanFVazquezOlivas@gmail.com>
2026-06-12 22:44:57 -0600

    Spark reading raw files

--- scripts/transform/transform_patients.py
+++ scripts/transform/transform_patients.py
@@ -1,12 +1,33 @@
 from pyspark.sql import SparkSession
+from pathlib import Path
+
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+RAW_PATIENT_PATH = PROJECT_ROOT/"data"/"raw"/"fhir_api"/"patient"
 
 def createSparkSesh() ->  SparkSession:
     return(SparkSession.builder.appName("Transformer").getOrCreate())
 
+
+
+
+
+
 def main():
     spark = createSparkSesh()
+    patientFiles = list(RAW_PATIENT_PATH.glob("*.json"))
+    rawBundleDF = (spark.read.option("multiline", "true").json([str(file) for file in patientFiles]))
+
+    rawBundleDF.printSchema()
+
+
     print(spark.version)
     spark.stop()
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
