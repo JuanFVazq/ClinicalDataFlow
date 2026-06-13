4affe2b02c7142987ee42b2b60e965f7b846ac2e
JuanFVO <JuanFVazquezOlivas@gmail.com>
2026-06-12 23:26:10 -0600

    Created initial pyspark transformation logic, WIP

--- scripts/transform/transform_patients.py
+++ scripts/transform/transform_patients.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import (col, explode)
 from pathlib import Path
 
 
@@ -18,8 +19,22 @@ def main():
     patientFiles = list(RAW_PATIENT_PATH.glob("*.json"))
     rawBundleDF = (spark.read.option("multiline", "true").json([str(file) for file in patientFiles]))
 
-    rawBundleDF.printSchema()
+    #rawBundleDF.printSchema()
 
+    patientResourcesDF = (rawBundleDF.select(explode(col("entry")).alias("entry"))).select(col("entry.resource").alias("patient"))
+
+    patients = patientResourcesDF.select(
+        col("patient.id").alias("patient_id"),
+        col("patient.gender").alias("gender"),
+        col("patient.birthDate").alias("birth_date"),
+        col("patient.address")[0]["city"].alias("city"),
+        col("patient.address")[0]["state"].alias("state"),
+        col("patient.address")[0]["postalCode"].alias("postalCode"),
+        col("patient.maritalStatus.text").alias("marital_status"),
+        col("patient.deceasedDateTime").alias("deceased_datetime")
+    )
+    patients.printSchema()
+    patients.show(10)
 
     print(spark.version)
     spark.stop()
